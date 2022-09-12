9384557ebe9ef7643db6b2880921339b41e46c74
Derrick Holleman <derrickholleman@gmail.com>
2022-09-12 15:02:46 -0500

    install prettier and format

--- src/lib/components/__tests__/Home.test.ts
+++ src/lib/components/__tests__/Home.test.ts
@@ -1,12 +1,12 @@
-import "@testing-library/jest-dom";
-import { render } from "@testing-library/svelte";
-import Home from "../Home.svelte";
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/svelte';
+import Home from '../Home.svelte';
 
-describe("Home", () => {
-  it("should render", () => {
-    const { getByTestId } = render(Home);
+describe('Home', () => {
+    it('should render', () => {
+        const { getByTestId } = render(Home);
 
-    const pageTitle = getByTestId("page-title");
-    expect(pageTitle).toBeInTheDocument();
-  });
+        const pageTitle = getByTestId('page-title');
+        expect(pageTitle).toBeInTheDocument();
+    });
 });
